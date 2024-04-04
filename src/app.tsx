bad10c789003a7e3d0f302bb388bcdeaa629f1b2
Leonardo S. Sousa <33811388+Sousa-L@users.noreply.github.com>
2024-04-04 17:28:46 -0300

    Adicionado componentes principais
    Foi adicionado os arquivos de texto dos componentes principais com placeholders em seu conteúdo.

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,12 @@
+import { AttendeeList } from "./components/attendee-list";
+import { Header } from "./components/header";
+
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+      <AttendeeList />
+    </div>
   )
 }
